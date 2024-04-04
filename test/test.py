9bb9b5671825bf18448b51c3edb8ac07d4474949
HaseebLUMS <mh6218@nyu.edu>
2024-04-04 18:12:23 +0000

    added a test

--- test/test.py
+++ test/test.py
@@ -10,7 +10,7 @@ import time
 
 BYTES_ORDER = "little"
 
-class VsockStream:
+class TcpStreamClient:
     """Client"""
     def __init__(self, conn_tmo=5):
         self.conn_tmo = conn_tmo
@@ -18,7 +18,7 @@ class VsockStream:
 
     def connect(self, endpoint):
         """Connect to the remote endpoint"""
-        self.sock = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.settimeout(self.conn_tmo)
         self.sock.connect(endpoint)
 
@@ -31,12 +31,12 @@ class VsockStream:
         data = self.sock.recv(1024)
         curr = time.time_ns()
         delta = int(curr) - int.from_bytes(data, BYTES_ORDER)
-        self.records.append(delta/1000)
+        self.records.append(delta / 1000)
 
     def disconnect(self):
         """Close the client socket"""
         self.sock.close()
-        assert(len(self.records) % 2 )
+        assert(len(self.records) % 2)
         rec = sorted(self.records)
         print("For ", len(self.records), " samples, median = ", rec[int(len(rec)/2)])
         mean = sum(rec) / len(rec)
@@ -45,8 +45,8 @@ class VsockStream:
         print("95th: ", rec[round((95/100)*(len(rec)))])
 
 def client_handler(args):
-    client = VsockStream()
-    endpoint = (args.cid, args.port)
+    client = TcpStreamClient()
+    endpoint = (args.host, args.port)
     client.connect(endpoint)
     for i in range(1001):
         time.sleep(0.002)
@@ -54,26 +54,25 @@ def client_handler(args):
     client.disconnect()
 
 
-class VsockListener:
+class TcpListener:
     """Server"""
     def __init__(self, conn_backlog=128):
         self.conn_backlog = conn_backlog
 
     def bind(self, port):
         """Bind and listen for connections on the specified port"""
-        self.sock = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
-        self.sock.bind((socket.VMADDR_CID_ANY, port))
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind(('0.0.0.0', port))
         self.sock.listen(self.conn_backlog)
 
     def recv_data(self):
         """Receive data from a remote endpoint"""
         while True:
-            (from_client, (remote_cid, remote_port)) = self.sock.accept()
+            (from_client, _) = self.sock.accept()
             # Read 1024 bytes at a time
             while True:
                 try:
                     data = from_client.recv(1024)
-                    # from_client.sendall(int(time.time_ns()).to_bytes(10, BYTES_ORDER))
                     from_client.sendall(data)
                 except socket.error:
                     break
@@ -81,31 +80,23 @@ class VsockListener:
                     break
             from_client.close()
 
-    def send_data(self, data):
-        """Send data to a renote endpoint"""
-        while True:
-            (to_client, (remote_cid, remote_port)) = self.sock.accept()
-            to_client.sendall(data)
-            to_client.close()
-
-
 def server_handler(args):
-    server = VsockListener()
+    server = TcpListener()
     server.bind(args.port)
     server.recv_data()
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='vsock-sample')
+    parser = argparse.ArgumentParser(prog='tcp-sample')
     parser.add_argument("--version", action="version",
                         help="Prints version information.",
                         version='%(prog)s 0.1.0')
     subparsers = parser.add_subparsers(title="options")
 
     client_parser = subparsers.add_parser("client", description="Client",
-                                          help="Connect to a given cid and port.")
-    client_parser.add_argument("cid", type=int, help="The remote endpoint CID.")
-    client_parser.add_argument("port", type=int, help="The remote endpoint port.")
+                                          help="Connect to a given host and port.")
+    client_parser.add_argument("host", type=str, help="The remote host address.")
+    client_parser.add_argument("port", type=int, help="The remote port.")
     client_parser.set_defaults(func=client_handler)
 
     server_parser = subparsers.add_parser("server", description="Server",
